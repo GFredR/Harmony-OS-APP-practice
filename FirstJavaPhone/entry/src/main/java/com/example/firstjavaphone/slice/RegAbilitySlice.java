c26689edc7976668c5fa293bbbc4d6ab2b311539
GFredR <664626586@qq.com>
2021-01-08 16:10:30 +0800

    add somechange

--- FirstJavaPhone/entry/src/main/java/com/example/firstjavaphone/slice/RegAbilitySlice.java
+++ FirstJavaPhone/entry/src/main/java/com/example/firstjavaphone/slice/RegAbilitySlice.java
@@ -3,12 +3,18 @@ package com.example.firstjavaphone.slice;
 import com.example.firstjavaphone.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.Button;
+import ohos.agp.components.Checkbox;
+import ohos.agp.components.RadioContainer;
 import ohos.agp.components.TextField;
 import ohos.ai.cv.text.Text;
 
 public class RegAbilitySlice extends AbilitySlice {
     Text nameText;
-    TextField nameField;
+    TextField nameField, ageField, phoneField, addressField;
+    Button submitBtn, cancelBtn;
+    RadioContainer radioContainer;
+    Checkbox hobby1, hobby2, hobby3, hobby4;
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
@@ -17,7 +23,21 @@ public class RegAbilitySlice extends AbilitySlice {
 
     void initRegView() {
         nameField = (TextField) findComponentById(ResourceTable.Id_nameField);
+        ageField = (TextField) findComponentById(ResourceTable.Id_ageField);
+        phoneField = (TextField) findComponentById(ResourceTable.Id_nameField);
+        addressField = (TextField) findComponentById(ResourceTable.Id_nameField);
 
+        submitBtn = (Button) findComponentById(ResourceTable.Id_submit);
+        cancelBtn = (Button) findComponentById(ResourceTable.Id_cancel);
+
+        radioContainer = (RadioContainer) findComponentById(ResourceTable.Id_sexRadio);
+
+        hobby1 = (Checkbox) findComponentById(ResourceTable.Id_hobby1);
+        hobby2 = (Checkbox) findComponentById(ResourceTable.Id_hobby2);
+        hobby3 = (Checkbox) findComponentById(ResourceTable.Id_hobby3);
+        hobby4 = (Checkbox) findComponentById(ResourceTable.Id_hobby4);
+
+        
     }
 
     @Override
